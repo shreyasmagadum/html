6f3c77b56ec976af28cdb567a55f34546df68204
shreyasmagadum <shreyasmagadum7@gmail.com>
2025-03-01 13:40:12 +0530

    first.js

---
+++ js/first.js
@@ -0,0 +1,79 @@
+
+console.log('external js');
+
+let b='10';
+console.log(a===b);
+
+console.log(123+'acs');
+
+//declaration
+//var name 
+//initialisation
+//name= "Abhishek"
+//console.log(name);
+
+//re-declaration
+//var name 
+//re-initialisation
+//name='Shreyas'
+//console.log(name);
+
+
+//let
+//let place 
+//place="jabalpur"
+//console.log(place);
+
+//place="pune"
+//console.log(place);
+
+//const
+//const age= 18;
+
+
+//-----------------------------------------------------------//
+var name ="prajakta"
+console.log(name);
+
+{
+    //console.log(name);
+    var name ="shreyas"
+    console.log(name);
+}
+console.log(name);
+
+let place= "Nipani"
+console.log(place);
+{
+    console.log(place);
+    let place2="nagpur"
+    console.log(place2);
+}
+console.log(place);
+//-----------------------------------------------------------//
+
+//dats type
+//number
+let age = 18
+let height =5.5
+console.log(height);
+let income= 146874352487354687654;
+console.log(income);
+console.log(typeof age);
+
+//strings
+let name="abhi"
+console.log(name);
+let letter = "A"
+console.log(letter);
+let msg = "Hello Iam String Datatype"
+console.log(typeof msg);
+
+let a=10
+console.log(a);
+
+let isMarried=true
+console.log(typeof isMarried);
+
+let incomeOfMe=BigInt("87547474587427585444")
+console.log(typeof incomeOfMe);
